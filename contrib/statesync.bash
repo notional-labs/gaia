9af47bc6fb5821c463ceb13c4304f7679eeb572b
Jacob Gadikian <jacobgadikian@gmail.com>
2022-11-12 09:14:04 +0700

    update state sync

--- contrib/statesync.bash
+++ contrib/statesync.bash
@@ -8,11 +8,11 @@ export PATH=$PATH:~/go/bin
 
 
 # Install Gaia
-make install
+# make install
 
 # MAKE HOME FOLDER AND GET GENESIS
-gaiad init test 
-wget -O ~/.gaia/config/genesis.json https://cloudflare-ipfs.com/ipfs/Qmc54DreioPpPDUdJW6bBTYUKepmcPsscfqsfFcFmTaVig
+#gaiad init test 
+#wget -O ~/.gaia/config/genesis.json https://cloudflare-ipfs.com/ipfs/Qmc54DreioPpPDUdJW6bBTYUKepmcPsscfqsfFcFmTaVig
 
 INTERVAL=1000
 
@@ -31,7 +31,7 @@ echo "TRUST HASH: $TRUST_HASH"
 # export state sync vars
 export GAIAD_STATESYNC_ENABLE=true
 export GAIAD_P2P_MAX_NUM_OUTBOUND_PEERS=200
-export GAIAD_STATESYNC_RPC_SERVERS="https://cosmos-rpc.polkachu.com:443,https://cosmoshub-4.technofractal.com:443"
+export GAIAD_STATESYNC_RPC_SERVERS="https://rpc-cosmoshub-ia.cosmosia.notional.ventures:443,https://cosmos-rpc.polkachu.com:443,https://cosmos-rpc.polkachu.com:443"
 export GAIAD_STATESYNC_TRUST_HEIGHT=$BLOCK_HEIGHT
 export GAIAD_STATESYNC_TRUST_HASH=$TRUST_HASH
 export GAIAD_P2P_SEEDS="ade4d8bc8cbe014af6ebdf3cb7b1e9ad36f412c0@seeds.polkachu.com:14956"
